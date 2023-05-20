9ed9e397d56eb8fa74d3c967f062546bfea6bff0
PipouilleJohnson <killian.bouzera77150@gmail.com>
2023-05-20 14:22:11 +0200

    save 20052023 14h22

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+// const des boutons du panneau de control
 const skoleomPage = document.getElementById("skoleom-page");
 const marketPlace = document.getElementById("market-place");
 const statistiques = document.getElementById("statistiques");
 const offresAbonnements = document.getElementById("offres-abonnements");
 
+//  const du bouton des parametres
+const parametres = document.getElementById("bouton-parametres");
+
+// const du panneau parametres
+const ecranParametres = document.getElementById("ecran-parametres");
+
+// const des écrans de monetizer
 const ecranSkoleomPage = document.getElementById("ecran-skoleom-page");
 const ecranMarketPlace = document.getElementById("ecran-market-place");
 const ecranStatistiques = document.getElementById("ecran-statistiques");
 const ecranOffresAbonnments = document.getElementById("ecran-offres-abonnements");
 
-
+// clics sur les boutons du panneau gauche
 skoleomPage.addEventListener("click", () => {
     skoleomPage.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
     marketPlace.style.backgroundColor = "rgba(0, 0, 0, 0)";
@@ -19,6 +27,8 @@ skoleomPage.addEventListener("click", () => {
     ecranMarketPlace.style.visibility = "hidden";
     ecranStatistiques.style.visibility = "hidden";
     ecranOffresAbonnments.style.visibility = "hidden";
+
+    ecranParametres.style.visibility = "hidden"
 });
 
 marketPlace.addEventListener("click", () => {
@@ -31,6 +41,8 @@ marketPlace.addEventListener("click", () => {
     ecranMarketPlace.style.visibility = "visible";
     ecranStatistiques.style.visibility = "hidden";
     ecranOffresAbonnments.style.visibility = "hidden";
+
+    ecranParametres.style.visibility = "hidden"
 });
 
 statistiques.addEventListener("click", () => {
@@ -43,6 +55,8 @@ statistiques.addEventListener("click", () => {
     ecranMarketPlace.style.visibility = "hidden";
     ecranStatistiques.style.visibility = "visible";
     ecranOffresAbonnments.style.visibility = "hidden";
+
+    ecranParametres.style.visibility = "hidden"
 });
 
 offresAbonnements.addEventListener("click", () => {
@@ -55,4 +69,11 @@ offresAbonnements.addEventListener("click", () => {
     ecranMarketPlace.style.visibility = "hidden";
     ecranStatistiques.style.visibility = "hidden";
     ecranOffresAbonnments.style.visibility = "visible";
+
+    ecranParametres.style.visibility = "hidden"
 });
+
+// clic sur le bouton des parametres
+parametres.addEventListener("click", () => {
+    ecranParametres.style.visibility = "visible";
+})
